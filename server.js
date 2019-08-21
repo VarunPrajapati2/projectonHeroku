56530d0fb775cbc6e4bd57f4d673fed9890f7947
VarunPrajapati2 <vrunprajapati1996@gmail.com>
2019-08-21 15:19:49 +0530

    second commit12

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var http = require("http");
-
+const port =process.env.PORT || 9090
 http.createServer((req,res)=>
 
 {
 res.writeHead(202,{'content-type': 'text/html'});
 res.end("Hello! Welcome to My Node Server...\n");
 console.log("Hello Friends! Welcome to My Node Server...");
-}).listen(9090);
+});
+
+app.listen(port,(req,res)=>{
+    console.log('server runging on port no '+port)
+    })
